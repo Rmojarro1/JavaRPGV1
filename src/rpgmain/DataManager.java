6940c83bf3bdf10a2eeeeb49feb25b14ccbe5f4d
Rmojarro1 <48000819+Rmojarro1@users.noreply.github.com>
2019-06-02 19:07:03 -0700

    battlemanager

---
+++ src/rpgmain/DataManager.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpgmain;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author gamef
+ */
+public class DataManager {
+    
+    private String playerName; 
+    private int playerMaxHP; 
+    private int playerHP; 
+    private int playerAttack; 
+    private int playerMoney; 
+    Scanner keyboard = new Scanner(System.in);
+    
+    public DataManager()
+    {
+        
+    }
+    
+    public DataManager(DataManager copy)
+    {
+        playerName = copy.playerName; 
+        playerMaxHP = copy.playerMaxHP; 
+        playerHP = copy.playerHP; 
+        playerAttack = copy.playerAttack; 
+        playerMoney = copy.playerMoney; 
+    }
+    public void newSaveData() throws FileNotFoundException
+    { 
+        String name; 
+        name = JOptionPane.showInputDialog("Enter a name for your player."); 
+        PrintWriter outputFile = new PrintWriter("Data.txt"); 
+        playerName = name;
+        playerMaxHP = 5; 
+        playerHP = 5; 
+        playerAttack = 1; 
+        playerMoney = 10;
+        outputFile.println(playerName); 
+        outputFile.println(playerMaxHP);
+        outputFile.println(playerHP);
+        outputFile.println(playerAttack);
+        outputFile.println(playerMoney);
+        outputFile.close();
+    }
+    
+    public void saveData(String name, int maxHP, int currentHP, 
+            int attack, int money) throws FileNotFoundException
+    { 
+        
+        PrintWriter outputFile = new PrintWriter("Data.txt");  
+        outputFile.println(name); 
+        outputFile.println(maxHP);
+        outputFile.println(currentHP);
+        outputFile.println(attack);
+        outputFile.println(money);
+        outputFile.close();
+        
+        playerName = name; 
+        playerMaxHP = maxHP; 
+        playerHP = currentHP; 
+        playerAttack = attack; 
+        playerMoney = money; 
+    }
+    
+    public void loadData() throws FileNotFoundException
+    {
+        File file = new File("Data.txt"); 
+        Scanner inputFile = new Scanner(file); 
+        playerName = inputFile.nextLine();
+        playerMaxHP = inputFile.nextInt(); 
+        playerHP = inputFile.nextInt();
+        playerAttack = inputFile.nextInt();
+        playerMoney = inputFile.nextInt();
+        inputFile.close(); 
+    }
+    
+    public String getName()
+    {
+        return playerName; 
+    }
+    
+    public int[] getStats()
+    {
+        int[] stats = new int[4]; 
+        stats[0] = playerMaxHP; 
+        stats[1] = playerHP; 
+        stats[2] = playerAttack; 
+        stats[3] = playerMoney; 
+        return stats; 
+    }
+}
